ea51afd6deedbe1e499c26a1ecf8a3b8014141c1
Marco Pérez <marcodamianperez@gmail.com>
2021-03-22 12:37:41 -0300

    Finalizada la interfaz de la página publish.html

--- dist/js/class.js
+++ dist/js/class.js
@@ -1,6 +1,6 @@
 class Publication {
     constructor(vehicleInfo, sellerInfo, images, exchange, dates, highlighted, description) {
-        this.vehicleInfo = vehicleInfo; // {type: 'car', brand: 'fiat', model: 'palio', version: '1.8 HLX', fuel: 'nafta', year: 2004, transmission: 'manual', mileaje: 162000, price: 420000}
+        this.vehicleInfo = vehicleInfo; // {type: 'car', brand: 'fiat', model: 'palio', version: '1.8 HLX', fuel: 'nafta', year: 2004, transmission: 'manual', mileaje: 162000, price: 420000, brandNew: false}
         this.sellerInfo = sellerInfo; // {name: 'Martín Cáseres', phone: '+549254982101', email: 'martincaseres@gmail.com', location: {country: 'Argentina', 'province: Mendoza', city: 'Guaymallén', street: 'Paso de los Andes 721'}}
         this.images = images; // [image1, image2, image3, ...]
         this.exchange = exchange;
---
+++ dist/js/index.js
@@ -0,0 +1,11 @@
+const vehicleTypeContainer = document.getElementById('vehicleTypeContainer');
+vehicleTypeContainer.addEventListener('click', e => {
+    localStorage.clear();
+    if (e.target.classList.contains('cars')) {
+        localStorage.setItem('type', 'car')
+    } else if (e.target.classList.contains('pickups')) {
+        localStorage.setItem('type', 'pickup')
+    } else if (e.target.classList.contains('bikes')) {
+        localStorage.setItem('type', 'motorcycle')
+    }
+});
