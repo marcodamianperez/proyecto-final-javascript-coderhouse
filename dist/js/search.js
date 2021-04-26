e0b06920551dfd45352c3dfa29a621a3f73aa1bd
Marco Pérez <marcodamianperez@gmail.com>
2021-04-26 19:37:40 -0300

    Agregada la opción para filtrar vehículos por marca y a su vez por modelo

--- dist/js/search.js
+++ dist/js/search.js
@@ -1,6 +1,8 @@
 // VARIABLES GLOBALES
 const type = localStorage.getItem('type');
-const select = document.getElementById('brand');
+const selectBrand = document.getElementById('brand');
+const selectModel = document.getElementById('model');
+const selectFuel = document.getElementById('fuel');
 
 // publicaciones por defecto para que la app no esté vacía al inicio
 const defaultPublications = publications.filter(publication => publication.vehicleInfo.type === type);
@@ -49,7 +51,7 @@ let generateHtml = (filteredPublications = currentPublications) => {
 }
 
 // Función que genera dinámicamente el select con las marcas de los vehículos en función de la disponibilidad de los mismos publicados
-let generateSelect = () => {
+let generateSelectBrand = () => {
     let brands = [];
     currentPublications.forEach(publication => {
         brands.push(publication.vehicleInfo.brand);
@@ -61,7 +63,23 @@ let generateSelect = () => {
         options += `<option value="${brand}">${brand}</option>`;
     });
 
-    select.innerHTML += options;
+    selectBrand.innerHTML += options;
+}
+
+let generateSelectModel = filteredPublications => {
+    selectModel.innerHTML = null;
+    let models = [];
+    filteredPublications.forEach(publication => {
+        models.push(publication.vehicleInfo.model);
+    });
+    models = Array.from(new Set(models));
+
+    let options = '<option value="all">Mostrar todos</option>';
+    models.forEach(model => {
+        options += `<option value="${model}">${model}</option>`;
+    });
+
+    selectModel.innerHTML += options;
 }
 
 // Función que muestra el listado de vehículos disponibles, según lo que el usuario eligió ver (autos, utilitarios o motos)
@@ -71,7 +89,7 @@ let showPublications  = () => {
 }
 
 const filterByBrand = () => {
-    const brand = select.value;
+    const brand = selectBrand.value;
     const filteredPublications = currentPublications.filter(publication => publication.vehicleInfo.brand === brand);
 
     if (brand === 'all') {
@@ -81,12 +99,39 @@ const filterByBrand = () => {
         $('#showcase').html(html);
     }
 
+    generateSelectModel(filteredPublications);
+}
+
+const filterByModel = () => {
+    const brand = selectBrand.value;
+    const model = selectModel.value;
+
+    const filteredByBrand = currentPublications.filter(publication => publication.vehicleInfo.brand === brand);
+    const filteredByBrandAndModel = filteredByBrand.filter(publication => publication.vehicleInfo.model === model);
+
+    if (model === 'all') {
+        const html = generateHtml(filteredByBrand);
+        $('#showcase').html(html);
+    } else {
+        const html = generateHtml(filteredByBrandAndModel);
+        $('#showcase').html(html);
+    }
+}
+
+const filterByFuel = () => {
+    const fuel = selectFuel.value;
+    const filteredPublications = currentPublications.filter(publication => publication.vehicleInfo.fuel === fuel);
+
+    const html = generateHtml(filteredPublications);
+    $('#showcase').html(html);
 }
 
 // LISTENERS
-select.addEventListener('change', filterByBrand);
+selectBrand.addEventListener('change', filterByBrand);
+selectModel.addEventListener('change', filterByModel);
+selectFuel.addEventListener('change', filterByFuel);
 
 $(document).ready(function () {
-    generateSelect();
+    generateSelectBrand();
     showPublications();
 });
